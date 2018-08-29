a320e32c4411ef8c111c8e6528754e42f4b31cb2
Cyrus <cyrus.dargahi@splitthepot.se>
2018-08-29 10:35:59 +0200

    separate script for fiat service

--- bash-scripts/start-default.sh
+++ bash-scripts/start-default.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 pm2 start --only bcmonitor
-pm2 start --only fiatrateservice
 pm2 start --only bws
 
 pm2 logs
---
+++ bash-scripts/start-fiat.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pm2 start --only fiatrateservice
+
+pm2 logs
